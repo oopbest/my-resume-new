bb234ff780cf35d2e6962d8b5af7374874216d74
Setthawut Pimthong <oop_best@yahoo.com>
2025-03-22 16:15:19 +0700

    update v.1.0.4

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -9,12 +9,55 @@ const SECOND_PROJECT_SOURCE = "https://github.com/oopbest/mern-crash-course";
 
 const THIRD_PROJECT_URL = "https://topvalue.com";
 
+const FOURTH_PROJECT_URL = "https://mern-chat-app-yt-8xug.onrender.com/";
+const FOURTH_PROJECT_SOURCE = "https://github.com/oopbest/mern-chat-app";
+
 function Project() {
   return (
     <section id="projects" className="bg-base-100 py-16">
       <div className="container mx-auto">
         <h2 className="text-3xl text-center font-bold mb-8">Portfolio</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 px-4">
+          {/* Project Fourth */}
+          <div className="card bg-white shadow-xl hover:shadow-2xl transition-all">
+            <figure className="h-2/4">
+              <motion.img
+                src="/project-image-4.png"
+                alt="Homepage Screenshot"
+                className="w-full h-full object-cover rounded-lg shadow-lg"
+                initial={{ opacity: 0, scale: 0.8 }}
+                animate={{ opacity: 1, scale: 1 }}
+                transition={{ duration: 0.8 }}
+                whileHover={{ scale: 1.05 }}
+              />
+            </figure>
+            <div className="card-body">
+              <h3 className="card-title text-xl font-semibold">
+                Full-Stack Friendly Real-Time Chat App
+              </h3>
+              <p className="text-gray-600">
+                Tech Stacks: React, Tailwind CSS, Daisy UI, Node.js, Express,
+                MongoDB, Mongoose, Zustand, useContext, Socket.io, JWT
+              </p>
+              <div className="card-actions justify-center mt-4">
+                <a
+                  target="_blank"
+                  href={FOURTH_PROJECT_URL}
+                  className="btn btn-primary"
+                >
+                  View Project
+                </a>
+                <a
+                  target="_blank"
+                  href={FOURTH_PROJECT_SOURCE}
+                  className="btn btn-secondary"
+                >
+                  Source
+                </a>
+              </div>
+            </div>
+          </div>
+
           {/* Project One */}
           <div className="card bg-white shadow-xl hover:shadow-2xl transition-all">
             <figure className="h-2/4">
@@ -30,11 +73,11 @@ function Project() {
             </figure>
             <div className="card-body">
               <h3 className="card-title text-xl font-semibold">
-                Full-Stack Real-Time Chat App
+                Full-Stack Real-Time Chat App with Image Uploads
               </h3>
               <p className="text-gray-600">
                 Tech Stacks: React, Tailwind CSS, Daisy UI, Node.js, Express,
-                MongoDB, Mongoose, Zustand, Cloudinary
+                MongoDB, Mongoose, Zustand, Cloudinary, Socket.io, JWT
               </p>
               <div className="card-actions justify-center mt-4">
                 <a
